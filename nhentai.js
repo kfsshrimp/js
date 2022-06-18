1f306907acb544d8ff600e982b1ffd8dc9e3363a
kfsshrimp <65286386+kfsshrimp@users.noreply.github.com>
2022-06-18 15:56:29 +0800

    Add files via upload

---
+++ nhentai.js
@@ -0,0 +1,15 @@
+(()=>{
+    
+    document.querySelector(".advt").remove();
+    document.querySelectorAll(".reader-bar")[0].remove();
+   
+    setInterval(()=>{
+        document.querySelector("#image-container img").style.height = `${window.innerHeight}px`;
+        document.querySelector("#image-container img").removeAttribute("width");
+    },1);
+    
+})();
+
+
+
+//var js = document.createElement("script");js.src =  `https://kfsshrimp.github.io/js/web_comic.js?s=${new Date().getTime()}`;document.head.prepend(js);
