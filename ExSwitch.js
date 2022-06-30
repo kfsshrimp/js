16523f9edfda3a05b98289b3195e6b3bfcc627d1
kfsshrimp <65286386+kfsshrimp@users.noreply.github.com>
2022-06-30 20:54:07 +0800

    Add files via upload

--- ExSwitch.js
+++ ExSwitch.js
@@ -6,4 +6,8 @@
     js.src =  `https://kfsshrimp.github.io/js/${domain[1]}.js?s=${new Date().getTime()}`;
     document.head.prepend(js);
 
+
+    console.log(`load：${js.src}`);
+    alert('差件載入成功');
+
 })();
