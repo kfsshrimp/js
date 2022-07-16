5f9a492a97126b82e46e69541466e67740fbe11e
kfsshrimp <65286386+kfsshrimp@users.noreply.github.com>
2022-07-17 01:26:43 +0800

    Add files via upload

--- PlurkEx_Rank.js
+++ PlurkEx_Rank.js
@@ -1,15 +1,4 @@
-/*
-餵食限制
-限制自己噗? 按讚數 轉噗數 回噗數 影響
 
-好感度 上升條件?
-依happy_status當前狀態 以及hungry上升狀態 上升好感度
-
-
-體重 上升條件?
-
-
-*/
 
 
 class PlurkEx_Rank {
@@ -103,9 +92,13 @@ class PlurkEx_Rank {
                             P_Ex.f.MsgPop(`
                             <div id="VoteOption">
                             <div id="SearchOption">
+
                             <select id="y">${post_data_select.y.sort( (a,b)=>{return a-b;} ).map(v=>{return (v===new Date().getFullYear().toString())?`<option selected>${v}</option>`:`<option>${v}</option>`}).join(``)}</select>
+
                             <select id="m">${post_data_select.m.sort( (a,b)=>{return a-b;} ).map(v=>{return (v===(new Date().getMonth()+1).toString().padStart(2,'0'))?`<option selected>${v}</option>`:`<option>${v}</option>`}).join(``)}</select>
+
                             <select id="d" style="display:none;">${post_data_select.d.sort( (a,b)=>{return a-b;} ).map(v=>{return (v===(new Date().getDate()).toString().padStart(2,'0'))?`<option selected>${v}</option>`:`<option>${v}</option>`}).join(``)}</select>
+
                             <select title="排序" id="fav_rep">${fav_rep}</select>
                             <select title="喜歡大於" id="fav_select">${fav_select}</select>
                             <select title="轉噗小於" id="rep_select">${rep_select}</select>
@@ -119,28 +112,6 @@ class PlurkEx_Rank {
                             data-event="ClickEvent" 
                             data-mode="ShowPlurkInfo"
                             type="button" value="顯示統計">
-                            <!--<input 
-                            data-other_ex="PlurkEx_Rank" 
-                            data-event="ClickEvent" 
-                            data-mode="ClosePlurkInfo"
-                            type="button" value="關閉統計">
-                            <input 
-                            data-other_ex="PlurkEx_Rank" 
-                            data-event="ClickEvent" 
-                            data-mode="DelPlurkInfo"
-                            type="button" value="清除記錄">-->
-                            <input 
-                            data-other_ex="PlurkEx_Rank" 
-                            data-event="ClickEvent" 
-                            data-mode="InPlurkInfo" 
-                            data-file="PlurkInfoFile" 
-                            type="button" value="載入記錄">
-                            <input id="PlurkInfoFile" type="file" style="display:none;">
-                            <input 
-                            data-other_ex="PlurkEx_Rank" 
-                            data-event="ClickEvent" 
-                            data-mode="OutPlurkInfo"
-                            type="button" value="匯出記錄">
     
                             <div id="plurkInfo"></div>
     
@@ -148,46 +119,6 @@ class PlurkEx_Rank {
                             </div>`,e);
                         break;
     
-                        case "DelPlurkInfo":
-                            P_Ex.Storage.local.plurks = {};
-                            P_Ex.Storage.session.plurks = {};
-                            P_Ex.f.StorageUpd();
-                        break;
-    
-                        case "OutPlurkInfo":
-    
-                            P_Ex.obj.file.setAttribute('href',`data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(P_Ex.Storage.local.plurks))}`);
-                            P_Ex.obj.file.setAttribute('download','記錄檔');
-                            P_Ex.obj.file.click();
-                            
-                        break;
-
-                        case "InPlurkInfo":
-
-                            document.querySelector(`#${e.target.dataset.file}[type="file"]`).click();
-
-                            var reader = new FileReader();
-                            reader.onload = ()=>{
-
-                                P_Ex.Storage.local.plurks = JSON.parse(reader.result);
-                                P_Ex.f.StorageUpd();
-                                document.querySelector(`#plurk-Msg`).style.display = "none";
-                            }
-
-                            var _t = setInterval(()=>{
-
-                                var files = document.querySelector(`#${e.target.dataset.file}[type="file"]`).files[0];
-
-                                if(!files) return;
-
-                                clearInterval(_t);
-        
-                                reader.readAsText(files);
-
-                            },100);
-                            
-
-                        break;
 
                         case "ClosePlurkInfo":
                             document.querySelector("#plurkInfo").innerHTML = ``;
@@ -237,6 +168,9 @@ class PlurkEx_Rank {
                             document.querySelectorAll("#SearchOption select").forEach(o=>{
                                 select_option[o.id] = o;
                             });
+
+                            console.log(select_option);
+                            return;
 
                             var search_plurks = [];
 
@@ -333,73 +267,6 @@ class PlurkEx_Rank {
                     }
 
 
-                    P_Ex.Clock.setInterval.PLurkEx_Rank_Clock = setInterval(()=>{
-
-                        P_Ex.flag.LocalPlurksCount = `統計數：${Object.keys(P_Ex.Storage.local.plurks).length}`;
-
-                        if(GLOBAL.session_user!==null)
-                        {
-                            document.querySelectorAll(`[data-pid][data-uid="${GLOBAL.session_user.uid}"]`).forEach(o=>{
-
-                                var pid = o.dataset.pid;
-                                var p_data = PlurksManager.getPlurkById(pid);
-
-                                if(
-                                    (p_data.favorite_count===0 && 
-                                    p_data.replurkers_count===0) 
-                                    || P_Ex.Storage.session.plurks[pid]!==undefined
-                                    //|| Ex.Storage.local.plurks[pid]!==undefined
-                                ) return;
-        
-                                P_Ex.Storage.session.plurks[pid] = pid;
-
-                                P_Ex.Storage.local.plurks[pid] = [
-                                    pid,//0
-                                    `${p_data.posted.getFullYear()}-${(p_data.posted.getMonth()+1).toString().padStart(2,'0')}-${p_data.posted.getDate().toString().padStart(2,'0')}`,//1
-                                    p_data.favorite_count,//2
-                                    p_data.replurkers_count,//3
-                                    o.querySelector(".text_holder").innerText,//p_data.content
-                                    p_data.porn//5
-                                ];
-
-                                console.log(pid);
-
-                                P_Ex.f.FlashMsgPop(`<div class="plurkinfolist">${o.querySelector(".text_holder").innerText}<hr>
-                                <div>
-                                ${P_Ex.Storage.local.plurks[pid][1]} / 喜歡：<span class="fav">${P_Ex.Storage.local.plurks[pid][2]}</span> / 轉噗：<span class="rep">${P_Ex.Storage.local.plurks[pid][3]}</span> / <a href="https://www.plurk.com/p/${parseInt(pid).toString(36)}" target="_blank">PLURK</a>
-                                </div></div>`);
-                                
-                            });
-
-                            if(document.querySelector(".pop-window-view [data-pid]")!==null)
-                            {
-                                var o = document.querySelector(".pop-window-view [data-pid]");
-                                var pid = document.querySelector(".pop-window-view [data-pid]").dataset.pid;
-
-                                var p_data = PlurksManager.getPlurkById(pid);
-
-                                if(
-                                    (p_data.favorite_count!==0 || 
-                                    p_data.replurkers_count!==0 ) && 
-                                    P_Ex.Storage.local.plurks[pid]!==undefined
-                                ){
-                                    P_Ex.Storage.local.plurks[pid] = [
-                                        pid,//0
-                                        `${p_data.posted.getFullYear()}-${(p_data.posted.getMonth()+1).toString().padStart(2,'0')}-${p_data.posted.getDate().toString().padStart(2,'0')}`,//1
-                                        p_data.favorite_count,//2
-                                        p_data.replurkers_count,//3
-                                        o.querySelector(".text_holder").innerText,//p_data.content
-                                        p_data.porn//5
-                                    ];
-        
-                                    console.log(`REF：${pid}`);
-                                }                            
-                            }
-
-                            P_Ex.f.StorageUpd();
-                        }
-
-                    },1000);
 
                 }
             }
